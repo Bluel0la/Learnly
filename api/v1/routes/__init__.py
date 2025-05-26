0d3f50d6f6c4f25dbc257fc5ee769b7e4e064f91
Tomilola <83653154+Bluel0la@users.noreply.github.com>
2025-05-26 13:21:51 +0100

    Add flashcards route to API version one

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from api.v1.routes.authentication import auth
 from api.v1.routes.chat import chat
+from api.v1.routes.flashcards import flashcards
 api_version_one = APIRouter(prefix="/api/v1")
 
 api_version_one.include_router(auth)
 api_version_one.include_router(chat)
+api_version_one.include_router(flashcards)
